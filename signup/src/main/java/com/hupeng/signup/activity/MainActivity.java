02dc423542df131ec3198325cc0465aa3ea30752
jasonliang <547798685@qq.com>
2015-08-10 21:19:49 +0800

    gradle多渠道打包

--- signup/src/main/java/com/hupeng/signup/activity/MainActivity.java
+++ signup/src/main/java/com/hupeng/signup/activity/MainActivity.java
@@ -1,6 +1,10 @@
 package com.hupeng.signup.activity;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,11 +18,19 @@ import com.litesuits.http.response.handler.HttpModelHandler;
 
 
 public class MainActivity extends Activity {
+
+    public static String APPID = "";
+    public static String APPKEY = "";
+    public static String APP_VERSION = "";
+    public static String APP_CHANNEL_ID = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        initPackageInfo(MainActivity.this);
         Button registerBtn = (Button)findViewById(R.id.register);
         registerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,5 +55,31 @@ public class MainActivity extends Activity {
             public void onClick(View v) {
             }
         });
+    }
+
+
+    private void initPackageInfo(Context context)
+    {
+        PackageManager manager = context.getPackageManager();
+        try {
+            PackageInfo info = manager.getPackageInfo(context.getPackageName(), 0);
+            APP_VERSION = info.versionName; // 版本名，versionCode同理
+
+            ApplicationInfo appInfo = manager.getApplicationInfo(context.getPackageName(), PackageManager.GET_META_DATA);
+            Object object = appInfo.metaData.get("UMENG_CHANNEL");
+            if (object instanceof String)
+                APP_CHANNEL_ID = appInfo.metaData.getString("UMENG_CHANNEL");
+            else if (object instanceof Integer)
+                APP_CHANNEL_ID = appInfo.metaData.getInt("UMENG_CHANNEL", 0)+"";
+            else if (object instanceof Long)
+                APP_CHANNEL_ID = appInfo.metaData.getLong("UMENG_CHANNEL", 0)+"";
+
+            Log.d("APP_VERSION: " + APP_VERSION + " APP_CHANNEL_ID: " + APP_CHANNEL_ID);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (ClassCastException e) {
+            e.printStackTrace();
+        }
     }
 }
